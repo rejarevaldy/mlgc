98a5e74dd603451fcf1c27374f310b9076ec7d07
rejarevaldy <rejarevaldy30@gmail.com>
2024-05-19 14:32:32 +0800

    Fix

--- src/server/server.js
+++ src/server/server.js
@@ -7,8 +7,8 @@ const InputError = require('../exceptions/InputError');
 
 (async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: 8000,
+    host: '0.0.0.0',
     routes: {
       cors: {
         origin: ['*'],
