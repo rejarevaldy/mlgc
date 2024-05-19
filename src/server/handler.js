e54450819424329d300ced83bf3cd4eb2bba10e3
rejarevaldy <rejarevaldy30@gmail.com>
2024-05-19 16:21:31 +0800

    fix

--- src/server/handler.js
+++ src/server/handler.js
@@ -6,41 +6,42 @@ async function postPredict(request, h) {
   const { image } = request.payload;
   const { model } = request.server.app;
 
-  const { confidenceScore, label, explanation, suggestion } =
-    await predictClassification(model, image);
+  const { resultScore, result, suggestion } = await predictClassification(
+    model,
+    image
+  );
   const id = crypto.randomUUID();
   const createdAt = new Date().toISOString();
 
   const data = {
-    id: id,
-    result: label,
-    explanation: explanation,
-    suggestion: suggestion,
-    confidenceScore: confidenceScore,
-    createdAt: createdAt,
+    id,
+    result,
+    suggestion,
+    createdAt,
   };
 
   await storeData(id, data);
-
   return h
     .response({
       status: 'success',
       message:
-        confidenceScore > 99
+        resultScore > 99
           ? 'Model is predicted successfully.'
-          : 'Model is predicted successfully but under treshold. Please use the correct picture.',
+          : 'Model is predicted successfully but under threshold. Please use the correct picture',
       data,
     })
     .code(201);
 }
 
 async function getPredictHistories(request, h) {
-  const histories = (await predictionsCollection.get()).docs.map((doc) => doc.data());
+  const histories = (await predictionsCollection.get()).docs.map((doc) =>
+    doc.data()
+  );
   const data = histories.map((item) => ({
     id: item.id,
     history: item,
   }));
-  return h.response({ status: "success", data }).code(200);
-};
+  return h.response({ status: 'success', data }).code(200);
+}
 
 export default { postPredict, getPredictHistories };
